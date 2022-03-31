8f9790a32caa72f4a31b1d52794af67cb83c13e2
Bruce Becker <brucellino@protonmail.com>
2022-03-31 09:24:54 +0200

    feat: add droplets, origin CA and dns
    Signed-off-by: Bruce Becker <brucellino@protonmail.com>

--- .terraform.lock.hcl
+++ .terraform.lock.hcl
@@ -1,6 +1,28 @@
 # This file is maintained automatically by "terraform init".
 # Manual edits may be lost in future updates.
 
+provider "registry.terraform.io/cloudflare/cloudflare" {
+  version     = "3.11.0"
+  constraints = "3.11.0"
+  hashes = [
+    "h1:oTTfUShNNcDCOxlXP2XiNleQYHmWswesQFCuicwQztw=",
+    "zh:0dd08c3336b3198e30158b61605674eefbc0b8e331ad8f25322ce1889fd2d8a7",
+    "zh:196d80c7ae594f1b6140de02ecb101ec1afd7e45877be849ace73866f3fcb689",
+    "zh:37bc087b5e858a92faf03c994cfbc4c906b0afecb7df2ff25961b394f3013cc0",
+    "zh:3910c38a3f001879e67aede543ac1de44beab2249704fd016f51b14875815bed",
+    "zh:6092e395636b673b8ee26dce9356331ac6ceaa6b62de17203dd151a22b9d9858",
+    "zh:96167bd63b49df0d4921f30d81cda5162b03af2bd20a6c1da65ba15bd28a2d30",
+    "zh:c168cab43707b4acdb8366074802df630cc4427a7c2e55c9489cdf56907d23fc",
+    "zh:ccdde1cd64fbce75a9266e3df8a8f3dbd481cf72de53fa3a5fb15c78304843ea",
+    "zh:db850c7627a312065867896c2bf0266b187beb24f3f898849c28364682f0646c",
+    "zh:ddbd2d93f7a8ecd131b63a3336e5e1fed00258a9312c218f6fcf3e0f04733160",
+    "zh:e8f02ec1dbf8dc0bcb4fcc29441fde52900f2182f88e1544074f8fb646ae89db",
+    "zh:eef9d202238b76925e28fefd79621d4e5e9d3927cbcbce918222856300aa206b",
+    "zh:f0ad67f42c4d8f3d20ca4e357ead759f651ac4bfbe0cd5006099deead8316e85",
+    "zh:f1a2d6f4a26e193172b8c3a9411582ed3909df93b62314a1460b31d32a782bc6",
+  ]
+}
+
 provider "registry.terraform.io/digitalocean/digitalocean" {
   version     = "2.17.1"
   constraints = "~> 2.0, 2.17.1"
@@ -21,6 +43,44 @@ provider "registry.terraform.io/digitalocean/digitalocean" {
     "zh:ef7bf5d68e1e26890628e8cc7801bf85699ea354cf60a87c25b55484b8ef0363",
     "zh:f3022214c908c640a8d0f33a5bc1a85effc80068f7df6aebe3cf730e0648ab9f",
     "zh:fe2c450490a1be3c612db5719e1833e271e63f1f7581ba6c4b79432053e69bb7",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/consul" {
+  version     = "2.15.0"
+  constraints = "2.15.0"
+  hashes = [
+    "h1:tAb2gwW+oZ8/t2j7lExdqpNrxmaWsHbyA2crFWClPb0=",
+    "zh:0bd2a9873099d89bd52e9eee623dd20ccb275d1e2f750da229a53a4d5b23450c",
+    "zh:1c9f87d4d97b2c61d006c0bef159d61d2a661a103025f8276ebbeb000129f931",
+    "zh:25b73a34115255c464be10a53f2510c4a1db958a71be31974d30654d5472e624",
+    "zh:32fa31329731db2bf4b7d0f09096416ca146f05b58f4482bbd4ee0f28cefbbcc",
+    "zh:59136b73d3abe7cc5b06d9e12d123ad21298ca86ed49a4060a3cd7c2a28a74a1",
+    "zh:a191f3210773ca25c543a92f2d392b85e6a053d596293655b1f25b33eb843b4c",
+    "zh:b8b6033cf0687eadc1099f11d9fb2ca9429ff40c2d85bd6cb047c0f6bc5d5d8d",
+    "zh:bb7d67ed28aa9b28fc5154161af003383f940b2beda0d4577857cad700f39cd1",
+    "zh:be615288f59327b975532a1999deab60a022e6819fe80e5a32526155210ecbba",
+    "zh:de1e3d5c34eef87eb301e74717754babb6dc8e19e3a964919e1165c5a076a719",
+    "zh:eb8c61b20d8ce2bfff9f735ca8456a0d6368af13aa1f43866f61c70f88cc491c",
+  ]
+}
+
+provider "registry.terraform.io/hashicorp/tls" {
+  version     = "3.1.0"
+  constraints = "3.1.0"
+  hashes = [
+    "h1:fUJX8Zxx38e2kBln+zWr1Tl41X+OuiE++REjrEyiOM4=",
+    "zh:3d46616b41fea215566f4a957b6d3a1aa43f1f75c26776d72a98bdba79439db6",
+    "zh:623a203817a6dafa86f1b4141b645159e07ec418c82fe40acd4d2a27543cbaa2",
+    "zh:668217e78b210a6572e7b0ecb4134a6781cc4d738f4f5d09eb756085b082592e",
+    "zh:95354df03710691773c8f50a32e31fca25f124b7f3d6078265fdf3c4e1384dca",
+    "zh:9f97ab190380430d57392303e3f36f4f7835c74ea83276baa98d6b9a997c3698",
+    "zh:a16f0bab665f8d933e95ca055b9c8d5707f1a0dd8c8ecca6c13091f40dc1e99d",
+    "zh:be274d5008c24dc0d6540c19e22dbb31ee6bfdd0b2cddd4d97f3cd8a8d657841",
+    "zh:d5faa9dce0a5fc9d26b2463cea5be35f8586ab75030e7fa4d4920cd73ee26989",
+    "zh:e9b672210b7fb410780e7b429975adcc76dd557738ecc7c890ea18942eb321a5",
+    "zh:eb1f8368573d2370605d6dbf60f9aaa5b64e55741d96b5fb026dbfe91de67c0d",
+    "zh:fc1e12b713837b85daf6c3bb703d7795eaf1c5177aebae1afcf811dd7009f4b0",
   ]
 }
 
--- main.tf
+++ main.tf
@@ -1,9 +1,21 @@
 terraform {
   required_version = ">= 1.1.0"
   required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "3.11.0"
+    }
+    consul = {
+      source  = "hashicorp/consul"
+      version = "2.15.0"
+    }
     digitalocean = {
       source  = "digitalocean/digitalocean"
       version = "~> 2.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "3.1.0"
     }
     vault = {
       source  = "hashicorp/vault"
@@ -40,20 +52,71 @@ variable "lb_size_unit" {
   default     = 1
 }
 
+data "vault_generic_secret" "hashiathome" {
+  path = "kv/hashiathome"
+}
+
 data "vault_generic_secret" "do_token" {
   path = "kv/do"
 }
 
+# Look up ssh key secret in vault
+data "vault_generic_secret" "ssh_key" {
+  path = "digitalocean/ssh_key"
+}
+
+data "cloudflare_zone" "hashiathome" {
+  name = "hashiatho.me"
+}
 provider "digitalocean" {
   token             = data.vault_generic_secret.do_token.data["token"]
   spaces_access_id  = data.vault_generic_secret.do_token.data["access_key_hashi_at_home"]
   spaces_secret_key = data.vault_generic_secret.do_token.data["secret_key_hashi_at_home"]
 }
 
+provider "cloudflare" {
+  api_token            = data.vault_generic_secret.hashiathome.data["cloudflare_api_token"]
+  api_user_service_key = data.vault_generic_secret.hashiathome.data["cloudflare_origin_ca_key"]
+}
+
 module "test-space" {
   selected_region = "🇳🇱"
   bucket_name     = "terraform-backend-hah"
   source          = "github.com/hashi-at-home/tfmod-do-space"
+}
+
+# Generate Certificate request
+resource "tls_private_key" "csr" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+# generate certificate request
+resource "tls_cert_request" "consul" {
+  key_algorithm   = tls_private_key.csr.algorithm
+  private_key_pem = tls_private_key.csr.private_key_pem
+
+  subject {
+    common_name  = "consul-${terraform.workspace}.hashiatho.me"
+    organization = "Hashi At Home"
+
+  }
+}
+
+# Get cert from cloudflare
+resource "cloudflare_origin_ca_certificate" "consul" {
+  csr                = tls_cert_request.consul.cert_request_pem
+  hostnames          = ["consul-${terraform.workspace}.hashiatho.me"]
+  request_type       = "origin-rsa"
+  requested_validity = 7
+}
+
+resource "digitalocean_certificate" "cert" {
+  name              = "cloudflare-origin"
+  type              = "custom"
+  private_key       = tls_private_key.csr.private_key_pem
+  leaf_certificate  = cloudflare_origin_ca_certificate.consul.certificate
+  certificate_chain = cloudflare_origin_ca_certificate.consul.certificate
 }
 
 # Digital Ocean VPC for droplets
@@ -64,18 +127,17 @@ resource "digitalocean_vpc" "vpc" {
   ip_range    = "10.10.10.0/24"
 }
 
-
-
-resource "digitalocean_ssh_key" "test_instance" {
+resource "digitalocean_ssh_key" "consul" {
   name       = var.ssh_key_name
-  public_key = file(var.ssh_key_path)
+  public_key = data.vault_generic_secret.ssh_key.data["public_key"]
 }
 
 resource "digitalocean_loadbalancer" "consul" {
-  name      = var.lb_name
-  size_unit = var.lb_size_unit
-  region    = "ams3"
-  vpc_uuid  = digitalocean_vpc.vpc.id
+  name        = var.lb_name
+  size_unit   = var.lb_size_unit
+  region      = "ams3"
+  vpc_uuid    = digitalocean_vpc.vpc.id
+  droplet_tag = "consul-server"
   forwarding_rule {
     entry_port     = "80"
     entry_protocol = "http"
@@ -84,10 +146,49 @@ resource "digitalocean_loadbalancer" "consul" {
     target_protocol = "http"
   }
 
+  # HTTPS forwarding rule
+  forwarding_rule {
+    entry_port     = "443"
+    entry_protocol = "https"
+
+    target_port     = 8500
+    target_protocol = "http"
+
+    certificate_name = digitalocean_certificate.cert.name
+  }
+
   healthcheck {
     port     = 8500
     protocol = "http"
     path     = "/v1/health/service/consul"
   }
 
+}
+
+resource "cloudflare_record" "consul" {
+  zone_id = data.cloudflare_zone.hashiathome.id
+  name    = "consul-${terraform.workspace}"
+  value   = digitalocean_loadbalancer.consul.ip
+  type    = "A"
+  ttl     = 60
+
+}
+
+data "digitalocean_image" "consul_server" {
+  name = "consul-server"
+}
+
+# Create consul server cluster with 3 nodes
+resource "digitalocean_droplet" "consul_server" {
+  count             = 3
+  name              = "consul-server-${terraform.workspace}-${count.index}"
+  image             = data.digitalocean_image.consul_server.id
+  region            = "ams3"
+  ssh_keys          = [digitalocean_ssh_key.consul.id]
+  size              = "s-1vcpu-1gb"
+  backups           = false
+  monitoring        = false
+  vpc_uuid          = digitalocean_vpc.vpc.id
+  tags              = ["consul-server"]
+  graceful_shutdown = false
 }
